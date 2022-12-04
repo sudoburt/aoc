c733f4588751c8141ed8e350d56660ecee408027
sudoburt <sudoburt@proton.me>
2022-12-04 00:32:29 -0500

    Complete day 4

---
+++ 2022/day04a/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    let input: Vec<&str> = include_str!("../input")
+        .lines()
+        .collect();
+
+    let mut total = 0;
+
+    for line in input {
+        let line: Vec<&str> = line.split(',').collect();
+
+        let first: Vec<u32> = line[0].split('-')
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+
+        let second: Vec<u32> = line[1].split('-')
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+
+        if (first[0] <= second[0] && first[1] >= second[1]) ||
+            (first[0] >= second[0] && first[1] <= second[1]) {
+            total += 1;
+        }
+    }
+
+    println!("The total number of overlapping sections is {}", total);
+
+}
